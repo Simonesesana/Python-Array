d98c5abcdcadfdcbeafd76a0c648eb9572fb2357
Simone Sesana <59932109+Simonesesana@users.noreply.github.com>
2020-01-19 17:29:52 +0100

    Update Array.py

--- Array.py
+++ Array.py
@@ -39,3 +39,18 @@ def low(arr):
         return("Error: invalid element in the array")
 
     return(l)
+
+def avg(arr):
+
+    a = 0               #Average value
+    i = 0               #Counter
+    
+    try:
+        while (i < len(arr)):
+            a = a + arr[i]
+            i = i + 1
+
+    except:
+        return("Error: invalid element in the array")
+
+    return(a/i)
