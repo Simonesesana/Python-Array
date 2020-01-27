410f67eadd3958ef0fb0a58a239b5a8e86949f0c
Simone Sesana <59932109+Simonesesana@users.noreply.github.com>
2020-01-27 17:28:44 +0100

    Update Arr.py

--- Arr.py
+++ Arr.py
@@ -68,3 +68,23 @@ def ema(n):
     
     except:
         return("Invalid argument in the function call")
+
+def rmv(arr, x):
+    
+    i = 0               #Counter
+    a = []              #Array
+    t = 0               #Temp var
+
+    try:
+        while (i < len(arr)):
+            t = arr[i]
+            i = i + 1
+        
+            if t == x:
+                pass
+            else:
+                a.append(t)
+        return(a)
+    
+    except:
+        return("Error: invalid argument in the function call")
